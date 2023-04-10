898e67eb28a9d0f320cdea5a2a665ac1c0aff3c3
AynurZaydullin <zaydullin.aynur@yandex.ru>
2023-04-10 09:23:28 +0300

    Initial commit.

--- src/main/java/EmployeeDAO.java
+++ src/main/java/EmployeeDAO.java
@@ -1,13 +1,3 @@
-import java.util.List;
 
 public interface EmployeeDAO {
-    List<Employee> getAllEmployees();
-
-    Employee getEmployeeById(long id);
-
-    void createEmployee(String firstName, String lastName, String gender, int age, int cityId);
-
-    void updateEmployee(long id, String firstName, String lastName, String gender, int age, int cityId);
-
-    void deleteEmployee(long id);
 }
